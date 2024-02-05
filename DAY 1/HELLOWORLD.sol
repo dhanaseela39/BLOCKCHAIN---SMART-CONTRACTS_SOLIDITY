dce772fc77433da3bfc648d1ce59d83d8c9ac07f
dhanaseela39 <146423828+dhanaseela39@users.noreply.github.com>
2024-02-05 23:59:53 +0530

    Update HELLOWORLD.sol

--- DAY 1/HELLOWORLD.sol
+++ DAY 1/HELLOWORLD.sol
@@ -1,8 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
-
-contract HelloWorld {
-    function getMessage() public pure returns (string memory) {
+contract HelloWorld
+{
+    function getMessage() public pure returns (string memory) 
+{
         return "Hello, World!";
     }
 }
